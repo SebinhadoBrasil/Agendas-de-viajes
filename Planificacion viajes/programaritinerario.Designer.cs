255d2b236b2b224f560facb0d2002aeb4ab4feb6
Arturo Ivan Armas Valderrama <N00382426@upn.pe>
2024-10-30 18:15:20 -0500

    mover

--- Planificacion viajes/programaritinerario.Designer.cs
+++ Planificacion viajes/programaritinerario.Designer.cs
@@ -46,9 +46,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(80, 35);
+            this.label1.Location = new System.Drawing.Point(60, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(299, 42);
+            this.label1.Size = new System.Drawing.Size(242, 34);
             this.label1.TabIndex = 0;
             this.label1.Text = "Lugares pendientes";
             // 
@@ -56,55 +57,57 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(768, 35);
+            this.label2.Location = new System.Drawing.Point(576, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(270, 42);
+            this.label2.Size = new System.Drawing.Size(218, 34);
             this.label2.TabIndex = 1;
             this.label2.Text = "Lugares visitados";
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(223, 120);
+            this.listBox1.Location = new System.Drawing.Point(167, 98);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(241, 116);
+            this.listBox1.Size = new System.Drawing.Size(182, 95);
             this.listBox1.TabIndex = 2;
             // 
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 16;
-            this.listBox2.Location = new System.Drawing.Point(877, 109);
+            this.listBox2.Location = new System.Drawing.Point(658, 89);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(296, 148);
+            this.listBox2.Size = new System.Drawing.Size(223, 121);
             this.listBox2.TabIndex = 3;
             // 
             // listBox3
             // 
             this.listBox3.FormattingEnabled = true;
-            this.listBox3.ItemHeight = 16;
-            this.listBox3.Location = new System.Drawing.Point(223, 397);
+            this.listBox3.Location = new System.Drawing.Point(167, 323);
+            this.listBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(250, 132);
+            this.listBox3.Size = new System.Drawing.Size(188, 108);
             this.listBox3.TabIndex = 4;
             // 
             // listBox4
             // 
             this.listBox4.FormattingEnabled = true;
-            this.listBox4.ItemHeight = 16;
-            this.listBox4.Location = new System.Drawing.Point(877, 397);
+            this.listBox4.Location = new System.Drawing.Point(658, 323);
+            this.listBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox4.Name = "listBox4";
-            this.listBox4.Size = new System.Drawing.Size(296, 148);
+            this.listBox4.Size = new System.Drawing.Size(223, 121);
             this.listBox4.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(60, 329);
+            this.label3.Location = new System.Drawing.Point(45, 267);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(351, 42);
+            this.label3.Size = new System.Drawing.Size(285, 34);
             this.label3.TabIndex = 6;
             this.label3.Text = "Actividades pendientes";
             // 
@@ -112,50 +115,55 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(768, 329);
+            this.label4.Location = new System.Drawing.Point(576, 267);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(338, 42);
+            this.label4.Size = new System.Drawing.Size(275, 34);
             this.label4.TabIndex = 7;
             this.label4.Text = "Actividades realizadas";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(67, 137);
+            this.textBox1.Location = new System.Drawing.Point(50, 111);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 22);
+            this.textBox1.Size = new System.Drawing.Size(92, 20);
             this.textBox1.TabIndex = 8;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(67, 411);
+            this.textBox2.Location = new System.Drawing.Point(50, 334);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(121, 22);
+            this.textBox2.Size = new System.Drawing.Size(92, 20);
             this.textBox2.TabIndex = 9;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(589, 165);
+            this.button1.Location = new System.Drawing.Point(437, 132);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(92, 29);
+            this.button1.Size = new System.Drawing.Size(69, 24);
             this.button1.TabIndex = 10;
             this.button1.Text = "visitado";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(589, 452);
+            this.button2.Location = new System.Drawing.Point(442, 356);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(86, 27);
+            this.button2.Size = new System.Drawing.Size(64, 22);
             this.button2.TabIndex = 11;
             this.button2.Text = "visitado";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // programaritinerario
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1260, 647);
+            this.ClientSize = new System.Drawing.Size(945, 526);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
@@ -168,6 +176,7 @@
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "programaritinerario";
             this.Text = "  ";
             this.ResumeLayout(false);
