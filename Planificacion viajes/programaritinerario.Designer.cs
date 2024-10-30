d9989eb663b0b1a8d58f035f02c47ba39a979500
MurginhaSebastian <vasodete856@gmail.com>
2024-10-30 18:23:16 -0500

    cambie a actividades a realizar

--- Planificacion viajes/programaritinerario.Designer.cs
+++ Planificacion viajes/programaritinerario.Designer.cs
@@ -110,9 +110,9 @@
             this.label3.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(60, 329);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(351, 42);
+            this.label3.Size = new System.Drawing.Size(327, 42);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Actividades pendientes";
+            this.label3.Text = "Actividades a realizar";
             // 
             // label4
             // 
