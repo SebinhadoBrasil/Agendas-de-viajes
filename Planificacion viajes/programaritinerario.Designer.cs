1fb56586524d697dbc561cf71def78d5c3c5d6ce
Arturo Ivan Armas Valderrama <N00382426@upn.pe>
2024-10-30 20:24:31 -0500

    adsfd

--- Planificacion viajes/programaritinerario.Designer.cs
+++ Planificacion viajes/programaritinerario.Designer.cs
@@ -183,7 +183,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(437, 437);
+            this.button5.Location = new System.Drawing.Point(437, 432);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 14;
