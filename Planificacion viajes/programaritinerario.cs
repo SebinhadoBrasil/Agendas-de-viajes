afde9c0e9398f78976eb7140d65f3f0446b8d7fb
MurginhaSebastian <vasodete856@gmail.com>
2024-11-07 10:47:33 -0500

    rama programa

--- Planificacion viajes/programaritinerario.cs
+++ Planificacion viajes/programaritinerario.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,13 @@ using System.Windows.Forms;
 namespace Planificacion_viajes
 {
     public partial class programaritinerario : Form
+
     {
+        private const string rutaArchivo2 = "Lugares pendientes.txt";
         public programaritinerario()
         {
             InitializeComponent();
+            CargarLugaresP();
         }
 
         private void textBox5_TextChanged(object sender, EventArgs e)
@@ -24,6 +28,50 @@ namespace Planificacion_viajes
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string LugaresP = textBox1.Text.Trim();
+
+            if (!string.IsNullOrEmpty(LugaresP))
+            {
+                if (!listBox1.Items.Contains(LugaresP))
+                {
+                    listBox1.Items.Add(LugaresP);
+                    GuardarLugaresPe(LugaresP);
+                    textBox1.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Este lugar pendiente ya ha sido agregado.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingresa un lugar pendiente.");
+            }
+
+
+
+
+        }
+        private void CargarLugaresP()
+        {
+            if (File.Exists(rutaArchivo2))
+            {
+                string[] LugaresP = File.ReadAllLines(rutaArchivo2);
+                listBox1.Items.AddRange(LugaresP);
+            }
+        }
+        private void GuardarLugaresPe(string LugaresP)
+        {
+            using (StreamWriter writer = new StreamWriter(rutaArchivo2, true))
+            {
+                writer.WriteLine(LugaresP);
+            }
         }
     }
+    
 }
